ba9ebab6cdbeab89b3cba915f4d6ea72d4fc9aac
andrzej-t <andrzej.tyrpa@gmail.com>
2021-06-14 12:32:42 +0200

    Changes up to exercise 22.2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,5 +1,6 @@
 package com.crud.tasks;
 
+import com.crud.tasks.controller.TrelloController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
