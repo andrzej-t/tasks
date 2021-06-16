e4ba79957715d3cf86447baea1d3e3aa7f2b5712
andrzej-t <andrzej.tyrpa@gmail.com>
2021-06-16 12:18:41 +0200

    Added classes: Badges, AttachmentsByType, Trello

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,11 +1,11 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
@@ -17,18 +17,35 @@ public final class TrelloController {
 
     private final TrelloClient trelloClient;
 
+    @PostMapping("createTrelloCard")
+    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return trelloClient.createNewCard(trelloCardDto);
+    }
 
     @GetMapping("getTrelloBoards")
     public void getTrelloBoards() {
 
+        // GET request
         final List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
         trelloBoards.stream()
-                .filter(p->Objects.nonNull(p.getId()) && Objects.nonNull(p.getName()))
-                .filter(p -> p.getName().contains("Kodilla"))
-                .forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+                .filter(p->Objects.nonNull(p.getId()) && Objects.nonNull(p.getName())).filter(p -> p.getName().contains("Kodilla"))
+            .forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
+            System.out.println("This board contains lists: ");
+            trelloBoardDto.getLists().forEach(trelloList -> {
+                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
+            });
         });
+
+//        final List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+//
+//        trelloBoards.stream()
+//                .filter(p->Objects.nonNull(p.getId()) && Objects.nonNull(p.getName()))
+//                .filter(p -> p.getName().contains("Kodilla"))
+//                .forEach(trelloBoardDto -> {
+//            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+//        });
 
     }
 
