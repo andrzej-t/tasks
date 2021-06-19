e4851992898da93c286a86e31ca960dd1e83382e
andrzej-t <andrzej.tyrpa@gmail.com>
2021-06-19 10:26:19 +0200

    Preperings to exercise 22.4

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/v1/trello")
@@ -23,40 +22,7 @@ public final class TrelloController {
     }
 
     @GetMapping("getTrelloBoards")
-    public void getTrelloBoards() {
-
-        // GET request
-        final List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        trelloBoards.stream()
-                .filter(p->Objects.nonNull(p.getId()) && Objects.nonNull(p.getName())).filter(p -> p.getName().contains("Kodilla"))
-            .forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
-            System.out.println("This board contains lists: ");
-            trelloBoardDto.getLists().forEach(trelloList -> {
-                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
-            });
-        });
-
-//        final List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-//
-//        trelloBoards.stream()
-//                .filter(p->Objects.nonNull(p.getId()) && Objects.nonNull(p.getName()))
-//                .filter(p -> p.getName().contains("Kodilla"))
-//                .forEach(trelloBoardDto -> {
-//            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-//        });
-
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
-
-//    @GetMapping("getTrelloBoards")
-//    public void getTrelloBoards() {
-//
-//        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-//
-//                trelloBoards.forEach(trelloBoardDto -> {
-//            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-//        });
-//    }
-
 }
