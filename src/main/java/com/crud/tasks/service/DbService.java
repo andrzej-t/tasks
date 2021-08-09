fed7f8d163d9f5f73d57fb075c5d70301bba44f5
andrzej-t <andrzej.tyrpa@gmail.com>
2021-08-09 17:00:59 +0200

    Exercise 29.3. TaskControllerTestSuite

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -3,6 +3,7 @@ package com.crud.tasks.service;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
