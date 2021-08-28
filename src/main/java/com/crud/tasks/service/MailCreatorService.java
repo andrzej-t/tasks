d7fef486ecd0175e724a0cfeaf9ff8886aacc932
andrzej-t <andrzej.tyrpa@gmail.com>
2021-08-28 17:46:50 +0200

    Exercise 32.2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -10,14 +11,20 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    AdminConfig adminConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
-//        context.setVariable("tasks_url", "http://localhost:8080/tasks_frontend");
-//        context.setVariable("button", "Visit website");
+        context.setVariable("prev_message", "Message generated automatically");
+        context.setVariable("tasks_url", "https://andrzej-t.github.io/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("admin_mail", adminConfig.getAdminMail());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
